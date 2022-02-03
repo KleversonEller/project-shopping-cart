37f67b4f091dc5fa60c495a254dc3844acc6346e
Kleverson Eller <kleverson.a16@gmail.com>
2022-02-02 22:25:37 -0300

    Requisito - 3

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  const pai = event.target.parentElement;
+  pai.removeChild(event.target);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
